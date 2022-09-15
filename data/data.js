c0f180f006d82d098ed44f7120647edb288c47af
brucalde <brunocsir@gmail.com>
2022-09-14 21:49:04 -0300

    Entrega prov

---
+++ data/data.js
@@ -0,0 +1,28 @@
+const products = [
+    {
+        "id": 1,
+        "title": "Mochila",
+        "price": 200,
+        "thumbnail": "https://foto.com/mochila"
+    },
+    {
+        "id": 2,
+        "title": "Televisor",
+        "price": 150,
+        "thumbnail": "https://foto.com/tv"
+    },
+    {
+        "id": 3,
+        "title": "Celular",
+        "price": 350,
+        "thumbnail": "https://foto.com/celular"
+    },
+    {
+        "id": 4,
+        "title": "Computadora",
+        "price": 50,
+        "thumbnail": "https://foto.com/compu"
+    }
+]
+
+module.exports = { products };
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+const apiRoutes = require('./router/app.router');
+const loggerMiddleware = require('./middlewares/logger');
+
+
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(loggerMiddleware);
+
+
+app.use( '/api', apiRoutes );
+
+
+
+
+const serverOn = app.listen(PORT, () => {
+    console.log(`Server connected on port ${PORT}`)
+});
+
+serverOn.on('error', (error) => {
+    console.log('error', (error))
+});
